aadbcca6328bfc8b5e25a45e93172dd00dc019a5
UIT2024003 <shravani.vetal@cumminscollege.in>
2026-01-20 12:35:56 +0530

    Created Initial Dashboard

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "60px" }}>
+      <h1>Welcome to Dashboard</h1>
+      <p>You have successfully logged in.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
